60e6ac06a1c476812edc65862c2a44bb3916194f
Katie Brown <katieleebrown19@gmail.com>
2022-09-04 21:35:38 -0400

    toggle button functional

--- public/js/main.js
+++ public/js/main.js
@@ -5,30 +5,30 @@ const backOfCard = document.querySelector('#back')
 
 document.querySelector('#hideBack').addEventListener('click', hideBack)
 
-function hideBack(){
-	front.classList.add('hidden')
-	back.classList.toggle('hidden')
+function hideBack() {
+    front.classList.add('hidden')
+    back.classList.toggle('hidden')
 }
 
-Array.from(deleteBtn).forEach((el)=>{
+Array.from(deleteBtn).forEach((el) => {
     el.addEventListener('click', deleteTodo)
 })
 
-Array.from(todoItem).forEach((el)=>{
+Array.from(todoItem).forEach((el) => {
     el.addEventListener('click', markComplete)
 })
 
-Array.from(todoComplete).forEach((el)=>{
+Array.from(todoComplete).forEach((el) => {
     el.addEventListener('click', markIncomplete)
 })
 
-//delete a jAnkie card
-async function deleteTodo(){
+//delete a jAnki card
+async function deleteTodo() {
     const todoId = this.parentNode.dataset.id
-    try{
+    try {
         const response = await fetch('todos/deleteTodo', {
             method: 'delete',
-            headers: {'Content-type': 'application/json'},
+            headers: { 'Content-type': 'application/json' },
             body: JSON.stringify({
                 'todoIdFromJSFile': todoId
             })
@@ -36,18 +36,18 @@ async function deleteTodo(){
         const data = await response.json()
         console.log(data)
         location.reload()
-    }catch(err){
+    } catch (err) {
         console.log(err)
     }
 }
 
 //mark off a jAnki card if you have it memorized
-async function markComplete(){
+async function markComplete() {
     const todoId = this.parentNode.dataset.id
-    try{
+    try {
         const response = await fetch('todos/markComplete', {
             method: 'put',
-            headers: {'Content-type': 'application/json'},
+            headers: { 'Content-type': 'application/json' },
             body: JSON.stringify({
                 'todoIdFromJSFile': todoId
             })
@@ -55,18 +55,18 @@ async function markComplete(){
         const data = await response.json()
         console.log(data)
         location.reload()
-    }catch(err){
+    } catch (err) {
         console.log(err)
     }
 }
 
 //add a marked off jAnki card back to the deck if you didn't quite get it memorized
-async function markIncomplete(){
+async function markIncomplete() {
     const todoId = this.parentNode.dataset.id
-    try{
+    try {
         const response = await fetch('todos/markIncomplete', {
             method: 'put',
-            headers: {'Content-type': 'application/json'},
+            headers: { 'Content-type': 'application/json' },
             body: JSON.stringify({
                 'todoIdFromJSFile': todoId
             })
@@ -74,21 +74,22 @@ async function markIncomplete(){
         const data = await response.json()
         console.log(data)
         location.reload()
-    }catch(err){
+    } catch (err) {
         console.log(err)
     }
 }
 
+// Show Back Button / Toggle Hidden
+document.querySelectorAll('.displayButton').forEach(element => {
+    element.addEventListener('click', showOther)
+    console.log(`test ${element.getAttribute('data-id')}`)
+})
 
+function showOther(click) {
 
-const contestants = document.querySelectorAll('.contestant')
-
-Array.from(contestants).forEach(element => element.addEventListener('click', checkForRose))
-
-function checkForRose(click){
-	if(click.target.classList.contains('rose')){
-		document.querySelector('#nikki').classList.toggle('hidden')
-	}else{
-		alert("Wrong!");
-	}
+    let info = event.currentTarget.getAttribute('data-id')
+    console.log(`click at data-id: ${info}`)
+    document.querySelectorAll(`p[data-id='${info}']`).forEach(x => {
+        x.classList.toggle('hidden')
+    })
 }
