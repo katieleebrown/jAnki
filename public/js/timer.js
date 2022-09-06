5429a1879d377e8f6970aec0912e8f709d77c4d4
BrendonW250 <brendonwright16@gmail.com>
2022-09-05 22:03:35 -0400

    timer-functionality-update

--- public/js/timer.js
+++ public/js/timer.js
@@ -1,25 +1,39 @@
-let timeSecond = 90;
-const timeH = document.querySelector("h1");
+// let timeSecond = 90;
+// const timeH = document.querySelector("h1");
 
-displayTime(timeSecond);
+// displayTime(timeSecond);
 
-const countDown = setInterval(() => {
-  timeSecond--;
-  displayTime(timeSecond);
-  if (timeSecond == 0 || timeSecond < 1) {
-    endCount();
-    clearInterval(countDown);
-  }
-}, 1000);
+// const countDown = setInterval(() => {
+//   timeSecond--;
+//   displayTime(timeSecond);
+//   if (timeSecond == 0 || timeSecond < 1) {
+//     endCount();
+//     clearInterval(countDown);
+//   }
+// }, 1000);
 
-function displayTime(second) {
-  const min = Math.floor(second / 60);
-  const sec = Math.floor(second % 60);
-  timeH.innerHTML = `
-  ${min < 10 ? "0" : ""}${min}:${sec < 10 ? "0" : ""}${sec}
-  `;
+// function displayTime(second) {
+//   const min = Math.floor(second / 60);
+//   const sec = Math.floor(second % 60);
+//   timeH.innerHTML = `
+//   ${min < 10 ? "0" : ""}${min}:${sec < 10 ? "0" : ""}${sec}
+//   `;
+// }
+
+// function endCount() {
+//   timeH.innerHTML = "Time out";
+// }
+
+function timer(){
+  var sec = 30
+  var timer = setInterval(function(){
+    document.getElementById('clock').innerHTML = '00: ' + sec
+    sec--
+
+    if (sec < 0){
+      clearInterval(timer)
+    }
+  }, 1000)
 }
 
-function endCount() {
-  timeH.innerHTML = "Time out";
-}
+// window.onload = timer()
